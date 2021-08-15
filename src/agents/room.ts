72906b54e4a416bc8f3a7cce0a802c0dd686bd0d
O Hedrick <ryebread8403@gmail.com>
2021-08-15 18:34:37 +0000

    adjusted main to iterate over room agents.

--- src/agents/room.ts
+++ src/agents/room.ts
@@ -5,12 +5,13 @@ export class RoomAgent {
         Sources: Source[];
         Room: Room;
         Spawns: StructureSpawn[];
-        constructor(){
-            this.Room = Game.rooms["sim"]
+        constructor(roomname: string){
+            this.Room = Game.rooms[roomname]
             this.Sources = this.Room.find(FIND_SOURCES);
             this.Spawns = this.Room.find(FIND_MY_SPAWNS);
         }
         execute () {
             this.Spawns[0].spawnCreep([WORK, WORK, MOVE], Guid.create().toString())
+            this.Room.find(FIND_MY_CREEPS)
         }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ErrorMapper } from "utils/ErrorMapper";
 import {StackCollection as Stack, QueueCollection as Queue} from "utils/StackNQueue"
 import {RoomAgent} from "agents/room";
+import { forEach } from "lodash";
 declare global {
   /*
     Example types, expand on these or remove them and add your own.
@@ -19,18 +20,29 @@ declare global {
   interface CreepMemory {
     role: string;
     room: string;
-    working: boolean;
   }
 
   // Syntax for adding proprties to `global` (ex "global.log")
   namespace NodeJS {
     interface Global {
       log: any;
+      creeps: Creep[] | undefined;
+      roomAgents: RoomAgent[];
     }
   }
 }
-const simRoom = new RoomAgent();
-simRoom.execute();
+for (const room of Object.keys(Game.rooms)){
+  global.roomAgents.push(new RoomAgent(room));
+}
+// Start of my main loop
+function main(): void {
+  for (const room of global.roomAgents) {
+    room.execute();
+  }
+
+}
+// End of my main loop
+
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
